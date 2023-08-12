2a2ee0980b0b34f9f3f358e93184a37371c85e1d
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-08-11 21:42:07 -0600

    Terminé al componente de introducción

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,26 +65,31 @@ export default function Intro() {
       >
         <Link
           href="#contact" 
-          className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
             Contact me
-            <BsArrowRight />
+            <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition" />
         </Link>
+
         <a 
-          href=''
-          className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
+          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
+          href="/CV.pdf" 
+          download={true}
+        >
             Download CV 
-            <HiDownload />
+            <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
+
         <a 
           href="https://www.linkedin.com/in/adolfo-ram%C3%ADrez-31b733179/"
           target='_blank'
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full' >
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition' >
             <BsLinkedin />
         </a>
+
         <a 
           href="https://github.com/Adolf97"
           target='_blank'
-          className='bg-white p-3 text-gray-700 flex items-center gap-2 rounded-full text-[1.38rem]' >
+          className='bg-white p-3 text-gray-700 flex items-center gap-2 rounded-full text-[1.38rem] focus:scale-110 hover:scale-110 active:scale-105 transition' >
             <FaGithubSquare />
         </a>
       </motion.div>
