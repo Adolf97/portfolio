d1a146ad14d6fda64b32d9a963087d0baa9b0650
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-08-11 23:19:18 -0600

    Creé y agregué el componente de projects

---
+++ components/projects.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+    </section>
+  )
+}
