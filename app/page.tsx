b54ef42fce08885d091c7ed01a116f36f24e9c49
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-08-15 17:19:41 -0600

    i do not remember what i changed xd

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
+import Experience from "@/components/experience";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
+      <Contact />
     </main>
   )
 }
