307a80ed557bcd47d85982dcc289f82ce79c868e
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-08-16 09:41:53 -0600

    added toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 import ActiveSectionContextProvider from '@/context/active-section-context'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Toaster position='top-right' />
         </ActiveSectionContextProvider>
       </body>
     </html>
