168b8b0b1346618e5fc4bb87bccccefc5a94d01f
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-08-16 09:47:56 -0600

    Created the function to send the email from the contact form

---
+++ actions/sendEmail.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import React from "react";
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactForm from "@/email/contact-form";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  // Simple server-side validation
+  if(!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email"
+    };
+  }
+
+  if(!validateString(message, 5000)) {
+    return {
+      error: "Invalid message"
+    };
+  }
+
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "ramirez.desanc@gmail.com",
+      subject: "Message from portfolio web page",
+      reply_to: senderEmail as string,
+      react: React.createElement(ContactForm, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+
+  return {
+    data
+  }
+}
